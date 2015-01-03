4ea97ee739c351851ca298f813a8228c50bac2ba
Charles-P. Clermont <charles.pclermont@gmail.com>
2015-01-03 16:05:48 -0500

    with views, ga, social media and environments

---
+++ src/cpclermont/views/content.clj
@@ -0,0 +1,8 @@
+(ns cpclermont.views.content)
+
+(def contents
+  {:base-url "http://cpclermont.com"
+   :index-title "CP Clermont"
+   :index-desc "Charles-Philippe Clermont is a marketer-programmer unicorn."
+   :index-img "http://cpclermont.com/public/img/cpclermont.jpg"
+   :index-site-name "CP Clermont"})
