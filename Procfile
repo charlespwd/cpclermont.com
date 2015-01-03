53c1ff4388df1b1e40d06e71b84ca84b28cee92c
Charles-P. Clermont <charles.pclermont@gmail.com>
2015-01-03 11:57:14 -0500

    initial commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java $JVM_OPTS -cp target/cpclermont-standalone.jar clojure.main -m cpclermont.web
---
+++ test/cpclermont/web_test.clj
@@ -0,0 +1,6 @@
+(ns cpclermont.web-test
+  (:require [clojure.test :refer :all]
+            [cpclermont.web :refer :all]))
+
+(deftest first-test
+  (is false "Tests should be written"))
