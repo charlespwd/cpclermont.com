9dc2583a3513049a9ce21947822407de6a9b0c39
Charles-P. Clermont <charles.pclermont@gmail.com>
2015-01-20 14:24:59 -0500

    Plug in template with content

--- src/cpclermont/views/content.clj
+++ src/cpclermont/views/content.clj
@@ -6,12 +6,12 @@
     :footer-title "Let's get in touch!"
     :hero-description "I love to talk about software and selectively do consulting work."
     :hero-headline "I make, ship, write, and help businesses convert users."
+    :hero-cta "Start"
     :hero-title "Hi, I'm Charles"
     :index-desc "Charles-Philippe Clermont is a marketer-programmer unicorn."
     :index-img "http://cpclermont.com/public/img/cpclermont.jpg"
     :index-site-name "CP Clermont"
     :index-title "CP Clermont"
     :tag-line "Engineering marketing solutions"
-
     :livereload "<script src=\"//localhost:35729/livereload.js\"></script>"}
    k))
--- src/cpclermont/views/views.clj
+++ src/cpclermont/views/views.clj
@@ -6,23 +6,26 @@
 (p/set-resource-path! 
   (clojure.java.io/resource "templates"))
 
-#_(when (env :dev) (p/cache-off!))
+(when (env :dev) (p/cache-off!))
 
 (defn layout [m]
   (p/render-file "layout.html" m))
 
 (defn home []
+
   (p/render-file 
     "pages/index.html" 
     {:title              (contents :index-title)
      :desc               (contents :index-desc)
      :img                (contents :index-img)
      :url                (contents :base-url)
+     :body-classes       "homepage"
      :site-name          (contents :index-site-name)
      :hero-headline      (contents :hero-headline)
+     :hero-cta           (contents :hero-cta)
      :hero-description   (contents :hero-description)
      :hero-title         (contents :hero-title)
      :footer-description (contents :footer-description)
      :footer-title       (contents :footer-title)
-     :livereload (if-not (env :production)
-                   (contents :livereload))}))
+     :livereload         (if-not                        (env :production)
+                         (contents :livereload))}))
