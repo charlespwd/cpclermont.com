f1818d95a4ebc779ae65732321117238b19c06af
Charles-P. Clermont <charles.pclermont@gmail.com>
2015-01-21 11:22:08 -0500

    Add content-map function in viewsviews.

--- src/cpclermont/views/views.clj
+++ src/cpclermont/views/views.clj
@@ -3,7 +3,11 @@
             [cpclermont.views.content :refer [contents]]
             [environ.core :refer [env]]))
 
-(p/set-resource-path! 
+(defn defaults []
+  {:livereload (if-not (env :production)
+                 (contents :livereload))})
+
+(p/set-resource-path!
   (clojure.java.io/resource "templates"))
 
 (when (env :dev) (p/cache-off!))
@@ -11,21 +15,22 @@
 (defn layout [m]
   (p/render-file "layout.html" m))
 
-(defn home []
+(defn content-map
+  "Helper method for developer convinience. Instead of writing :key
+   (contents :key), this method does it automatically."
+  [ks custom]
+  (merge (defaults) (zipmap ks (map contents ks)) custom))
 
-  (p/render-file 
-    "pages/index.html" 
-    {:title              (contents :index-title)
-     :desc               (contents :index-desc)
-     :img                (contents :index-img)
-     :url                (contents :base-url)
-     :body-classes       "homepage"
-     :site-name          (contents :index-site-name)
-     :hero-headline      (contents :hero-headline)
-     :hero-cta           (contents :hero-cta)
-     :hero-description   (contents :hero-description)
-     :hero-title         (contents :hero-title)
-     :footer-description (contents :footer-description)
-     :footer-title       (contents :footer-title)
-     :livereload         (if-not                        (env :production)
-                         (contents :livereload))}))
+(defn home []
+  (p/render-file
+    "pages/index.html"
+    (content-map [:hero-headline
+                  :hero-cta
+                  :hero-title
+                  :footer-description
+                  :footer-title]
+                 {:title (contents :index-title)
+                  :desc  (contents :index-desc)
+                  :img   (contents :index-img)
+                  :url   (contents :base-url)
+                  :body-classes "homepage"})))
